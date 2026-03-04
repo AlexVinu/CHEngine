1d4f74e7342ddafed944357d03a2b782da947b01
marat <marat@MacBook-Air-marat.local>
2026-03-04 14:23:57 +0500

    Добавить менеджер шейдеров с горячей перезагрузкой
    - IShader: добавлен виртуальный метод Reload(vertSrc, fragSrc) для перекомпиляции шейдера на месте
    - ShaderOGL: логика компиляции вынесена в статический хелпер CompileProgram; Reload() пересобирает GPU-программу без смены дескриптора
    - RenderResourceManager: добавлены ShaderEntry, именной CreateShaderFromFile, ReloadShader и GetShaderEntries
    - Application: добавлен синглтон Get(), GetRenderResources(), GetActiveShader/SetActiveShader
    - Sandbox: новые шейдеры flat (оранжевый) и neon (сине-голубой градиент); ImGui-окно «Shader Manager» — переключение и перезагрузка шейдеров в рантайме

--- Modules/Rendering/RendererOGL/src/ShaderOGL.cpp
+++ Modules/Rendering/RendererOGL/src/ShaderOGL.cpp
@@ -1,21 +1,21 @@
 #include <chepch.h>
 #include "ShaderOGL.h"
 
-#include<glad/glad.h>
+#include <glad/glad.h>
 
 namespace CHModules
 {
-	ShaderOGL::ShaderOGL(const CHEngine::String& vertexSrc, const CHEngine::String& fragmentSrc)
-	{
-		// Create an empty vertex shader handle
-		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-		// Send the vertex shader source code to GL
-		// Note that std::string's .c_str is NULL character terminated.
+	// ---------------------------------------------------------------------------
+	// Static helper: compile + link a GLSL program.
+	// Returns the program ID on success, 0 on failure.
+	// ---------------------------------------------------------------------------
+	GLuint ShaderOGL::CompileProgram(const CHEngine::String& vertexSrc, const CHEngine::String& fragmentSrc)
+	{
+		// ---- vertex shader ----
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 		const GLchar* source = vertexSrc.c_str();
 		glShaderSource(vertexShader, 1, &source, 0);
-
-		// Compile the vertex shader
 		glCompileShader(vertexShader);
 
 		GLint isCompiled = 0;
@@ -24,28 +24,17 @@ namespace CHModules
 		{
 			GLint maxLength = 0;
 			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-			// The maxLength includes the NULL character
 			std::vector<GLchar> infoLog(maxLength);
-			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
-
-			// We don't need the shader anymore.
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, infoLog.data());
 			glDeleteShader(vertexShader);
-
-			CHE_CORE_ERROR("{0}", infoLog.data());
-			CHE_CORE_ASSERT(false, "Vertex shader compilation failure!");
-			return;
+			CHE_CORE_ERROR("Vertex shader compilation failed: {0}", infoLog.data());
+			return 0;
 		}
 
-		// Create an empty fragment shader handle
+		// ---- fragment shader ----
 		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-		// Send the fragment shader source code to GL
-		// Note that std::string's .c_str is NULL character terminated.
 		source = fragmentSrc.c_str();
 		glShaderSource(fragmentShader, 1, &source, 0);
-
-		// Compile the fragment shader
 		glCompileShader(fragmentShader);
 
 		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
@@ -53,60 +42,49 @@ namespace CHModules
 		{
 			GLint maxLength = 0;
 			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-			// The maxLength includes the NULL character
 			std::vector<GLchar> infoLog(maxLength);
-			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
-
-			// We don't need the shader anymore.
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, infoLog.data());
 			glDeleteShader(fragmentShader);
-			// Either of them. Don't leak shaders.
 			glDeleteShader(vertexShader);
-
-			CHE_CORE_ERROR("{0}", infoLog.data());
-			CHE_CORE_ASSERT(false, "Fragment shader compilation failure!");
-			return;
+			CHE_CORE_ERROR("Fragment shader compilation failed: {0}", infoLog.data());
+			return 0;
 		}
 
-		// Vertex and fragment shaders are successfully compiled.
-		// Now time to link them together into a program.
-		// Get a program object.
-		m_RendererID = glCreateProgram();
-		GLuint program = m_RendererID;
-
-		// Attach our shaders to our program
+		// ---- link program ----
+		GLuint program = glCreateProgram();
 		glAttachShader(program, vertexShader);
 		glAttachShader(program, fragmentShader);
-
-		// Link our program
 		glLinkProgram(program);
 
-		// Note the different functions here: glGetProgram* instead of glGetShader*.
 		GLint isLinked = 0;
-		glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
+		glGetProgramiv(program, GL_LINK_STATUS, &isLinked);
 		if (isLinked == GL_FALSE)
 		{
 			GLint maxLength = 0;
 			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
-
-			// The maxLength includes the NULL character
 			std::vector<GLchar> infoLog(maxLength);
-			glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
-
-			// We don't need the program anymore.
+			glGetProgramInfoLog(program, maxLength, &maxLength, infoLog.data());
 			glDeleteProgram(program);
-			// Don't leak shaders either.
-			glDeleteShader(vertexShader);
 			glDeleteShader(fragmentShader);
-
-			CHE_CORE_ERROR("{0}", infoLog.data());
-			CHE_CORE_ASSERT(false, "Shader link failure!");
-			return;
+			glDeleteShader(vertexShader);
+			CHE_CORE_ERROR("Shader link failed: {0}", infoLog.data());
+			return 0;
 		}
 
-		// Always detach shaders after a successful link.
 		glDetachShader(program, vertexShader);
 		glDetachShader(program, fragmentShader);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		return program;
+	}
+
+	// ---------------------------------------------------------------------------
+
+	ShaderOGL::ShaderOGL(const CHEngine::String& vertexSrc, const CHEngine::String& fragmentSrc)
+	{
+		m_RendererID = CompileProgram(vertexSrc, fragmentSrc);
+		CHE_CORE_ASSERT(m_RendererID, "ShaderOGL: failed to compile/link shader");
 	}
 
 	ShaderOGL::~ShaderOGL()
@@ -123,4 +101,16 @@ namespace CHModules
 	{
 		glUseProgram(0);
 	}
+
+	bool ShaderOGL::Reload(const CHEngine::String& vertexSrc, const CHEngine::String& fragmentSrc)
+	{
+		GLuint newProgram = CompileProgram(vertexSrc, fragmentSrc);
+		if (!newProgram)
+			return false;
+
+		glDeleteProgram(m_RendererID);
+		m_RendererID = newProgram;
+		return true;
+	}
+
 }
